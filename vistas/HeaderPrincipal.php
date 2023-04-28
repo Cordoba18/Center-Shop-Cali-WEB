12735a896ba0c76aec4f14673bffe7fa9eba432e
Jhan Carlos Cordoba <123204602+Cordoba18@users.noreply.github.com>
2023-04-28 09:08:33 -0500

    Remover librerias

--- vistas/HeaderPrincipal.php
+++ vistas/HeaderPrincipal.php
@@ -8,10 +8,7 @@ include_once("../sql/Conexion.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/Header.css">
-    <link href="../librerias/bootstrap.min.css" 
-    rel="stylesheet" 
-    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" 
-    crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg">
@@ -51,6 +48,6 @@ include_once("../sql/Conexion.php");
     </div>
   </div>
 </nav>
-<script src="../librerias/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
