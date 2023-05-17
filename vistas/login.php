7b940fdc207a6277071c14559d7086dee7603f96
Jhan Carlos Cordoba <123204602+Cordoba18@users.noreply.github.com>
2023-05-17 08:12:41 -0500

    Login melitico

--- vistas/login.php
+++ vistas/login.php
@@ -1,30 +1,25 @@
-<?php
-include_once("HeaderSimple.php");
-?>
+
 <head>
     <title> INICIO SESIÓN</title>
     <link rel="stylesheet" href="../css/Login.css">
 </head>
 <body>
-<div class="Login">
-
-
-
-
+<div class="Login"> 
 <form action="../controlador/LoginController.php" method="post">
 <h1> INICIAR SESION</h1>
+<div class="contenedor-inputs">
 <label >CORREO: </label>
-<input type="email" name="correo">
-<label >CONTRASEÑA:</label>
+<input type="email" name="correo"></div>
+<div class="contenedor-inputs">
+<label >CONTRASEÑA:</label></div>
 <input type="password" name="contrasena" >
-<input type="submit" value="INICIAR">
+<div class="contenedor-boton">
+<input type="submit" value="INICIAR"></div>
+<div class="contenedor-links">
 <a href="CrearCuenta.php">CREAR CUENTA</a>
-<a href="RestablecerContraseña.php">RECUPERAR CONTRASEÑA</a>
+<a href="RestablecerContraseña.php">RECUPERAR CONTRASEÑA</a></div>
 </form>
 </div>
 </body>
 
 </html>
-<?php
-include_once("FooderPrincipal.php");
-?>
